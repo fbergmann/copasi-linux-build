c6369b074d50aa77357b7420ca11bbb998fe3c13
Frank T. Bergmann <fbergman@caltech.edu>
2025-05-08 06:52:18 +0000

    correct path

--- scripts/5_build-copasi.sh
+++ scripts/5_build-copasi.sh
@@ -1,3 +1,3 @@
-cmake -G Ninja -B build-copasi -S COPASI -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=install-copasi -DSELECT_QT=Qt6 -DQt6_DIR=$Qt6_DIR -DCOPASI_DEPENDENCY_DIR=install-dependencies -DQt6Widgets_UIC_EXECUTABLE=/io/6.8.2/gcc_64/libexec/uic -DWITH_DATAVISUALIZATION_NAMESPACES=OFF
+cmake -G Ninja -B build-copasi -S COPASI -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_INSTALL_PREFIX=install-copasi -DSELECT_QT=Qt6 -DQt6_DIR=$Qt6_DIR -DCOPASI_DEPENDENCY_DIR=install-dependencies -DQt6Widgets_UIC_EXECUTABLE=$QT_VERSION/gcc_64/libexec/uic -DWITH_DATAVISUALIZATION_NAMESPACES=OFF
 cmake --build build-copasi
 cmake --install build-copasi
